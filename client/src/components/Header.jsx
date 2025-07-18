e99e0f531bd5698c6dfbc4acebe72139c1d7460c
Khaled Almelhem <kh.almelhem@mail.com>
2025-07-18 11:56:05 +0200

    haz test

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
-export default function Header(){
-    return(
-        <>
-            <div>
-                <h2>Header4</h2>
-            </div>
-        </>
-    )
+export default function Header() {
+  return (
+    <>
+      <div>
+        <h2>Header4</h2>
+        <h2>haz title </h2>
+      </div>
+    </>
+  );
 }
